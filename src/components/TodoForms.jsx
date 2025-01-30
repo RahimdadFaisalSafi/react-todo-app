559a7d60c18b57c056263d85179d078f6606531a
RahimdadFaisalSafi <rahimdadfaisal.safi@gmail.com>
2025-01-30 15:29:55 +0100

    Add TodoForm

---
+++ src/components/TodoForms.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const TodoForm = ({ onAddTodo }) => {
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const trimmedInput = input.trim();
+    if(!trimmedInput)
+        return;
+    onAddTodo(trimmedInput)
+    setInput('')
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Add new Todo"
+        value={input}
+        onChange={(e) => setInpute(e.target.value)}
+      />
+
+      <button onClick={handleSubmit}>Save</button>
+    </div>
+  );
+};
